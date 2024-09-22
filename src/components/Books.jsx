cd19054e708e72eab1d5b01bcbef737ca369cf8f
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-09-22 23:36:37 +0330

    It is possible to display the list of favorites

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react";
+
 import { books } from "../constants/mockData";
 import BookCard from "./BookCard";
+import SideCard from "./SideCard";
+
+import styles from "./Books.module.css"
 
 function Books() {
+  const [liked, setLiked] = useState([]);
+
+  const handleLikedList = (book, status) => {
+    if (status) {
+      const newLikedList = liked.filter((i) => i.id !== book.id);
+      setLiked(newLikedList);
+    } else {
+      setLiked((liked) => [...liked, book]);
+    }
+  };
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.cards}>
         {books.map((book) => (
-          <BookCard key={book.id} data={book}/>
+          <BookCard
+            key={book.id}
+            data={book}
+            handleLikedList={handleLikedList}
+          />
         ))}
       </div>
-      <div></div>
+      {!!liked.length && (
+        <div className={styles.favorite}>
+          <h4>Favorite</h4>
+          {liked.map((book) => (
+            <SideCard key={book.id} data={book} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
